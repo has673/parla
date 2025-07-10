833ed0d59d2746ae2904485bd217c5159d639070
has673 <hassansajjad096@gmail.com>
2025-07-10 11:10:13 +0500

    modal

--- Components/Layout/Sidebar.js
+++ Components/Layout/Sidebar.js
@@ -4,269 +4,111 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 
-import { useRouter } from "next/navigation";
-
 const Sidebar = () => {
-  const router = useRouter();
   const pathname = usePathname();
 
   const isActive = (paths) =>
     Array.isArray(paths) ? paths.includes(pathname) : pathname === paths;
 
+  const menuItems = [
+    {
+      label: "Home",
+      href: "/HomeScreen",
+      icon: "/images/home.png",
+      activeIcon: "/images/home.png",
+    },
+    {
+      label: "Explore",
+      href: "/UserDashboard/Quotation",
+      icon: "/images/explore.png",
+      activeIcon: "/images/quotation.svg",
+    },
+    {
+      label: "Appointments",
+      href: "/Appointment/Appointments",
+      icon: "/images/appoint.png",
+      activeIcon: "/images/appointwhite.png",
+      match: [
+        "/Appointment/Appointments",
+        "/Appointment/CancelAppointment",
+        "/Appointment/Waiting",
+        "/Appointment/SingleAppointment",
+        "/Appointment/AppointmentDetail",
+        "/Appointment/AppointentHistory",
+        "/BookAppointment/",
+      ],
+    },
+    {
+      label: "Favorite",
+      href: "/Favorite",
+      icon: "/images/Heart.png",
+      activeIcon: "/images/paymentblue.svg",
+    },
+    {
+      label: "Account",
+      href: "/Service/Account",
+      icon: "/images/Account.png",
+      activeIcon: "/images/accountwhite.png",
+    },
+    {
+      label: "Points",
+      href: "/Points",
+      icon: "/giftwhite.png",
+      activeIcon: "/gift.png",
+    },
+    {
+      label: "Stamp Cards",
+      href: "/StampCard",
+      icon: "/stampblack.png",
+      activeIcon: "/stampwhite.png",
+    },
+    {
+      label: "Gift Cards",
+      href: "/Gift",
+      icon: "/store.png",
+      activeIcon: "/storewgite.png",
+    },
+  ];
+
   return (
-    <div className="hidden md:flex flex-col w-1/5  bg-white h-auto border-r border-r-[#AAAAAA]">
-      <div>
-        <div className="flex-1 ">
-          {/* MENU Section */}
-          <div className="mb-6 py-2">
-            <ul>
-              <li
-                className={`mb-6   px-4  ${
-                  isActive(["/HomeScreen"]) &&
-                  "border-l border-l-[var(--orange)]"
-                } `}
-              >
-                <Link href="/HomeScreen">
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/HomeScreen")
-                        ? "bg-[var(--orange)] text-white mx-3"
-                        : "text-[#202224]  mx-3"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        src="/images/home.png"
-                        alt="Pet Profile icon"
-                        width={16}
-                        height={16}
-                        className="object-contain"
-                      />
+    <div className="hidden md:flex flex-col w-1/5 bg-white h-auto border-r border-r-[#AAAAAA]">
+      <div className="flex-1">
+        <div className="mb-6 py-2">
+          <ul>
+            {menuItems.map((item, index) => {
+              const active = isActive(item.match || item.href);
+
+              return (
+                <li
+                  key={index}
+                  className={`mb-6 px-4 ${
+                    active && "border-l border-l-[var(--orange)]"
+                  }`}
+                >
+                  <Link href={item.href}>
+                    <div
+                      className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
+                        active
+                          ? "bg-[var(--orange)] text-white mx-3"
+                          : "text-[#202224] mx-3"
+                      }`}
+                    >
+                      <div className="mr-3 w-5 h-5 relative">
+                        <Image
+                          src={active ? item.activeIcon : item.icon}
+                          alt={`${item.label} icon`}
+                          width={20}
+                          height={20}
+                          className="object-contain"
+                        />
+                      </div>
+                      <span>{item.label}</span>
                     </div>
-                    <span>Home</span>
-                  </div>
-                </Link>
-              </li>
-              <li
-                className={`mb-6   px-4  ${
-                  isActive(["/Explore"]) && "border-l border-l-[var(--orange)]"
-                } `}
-              >
-                <Link href="/UserDashboard/Quotation">
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/UserDashboard/PainScore")
-                        ? "bg-[var(--orange)] text-white mx-3"
-                        : "text-[#202224]  mx-3"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        src={
-                          isActive("/UserDashboard/Quotation")
-                            ? "/images/quotation.svg"
-                            : "/images/explore.png"
-                        }
-                        alt="Pet Profile icon"
-                        width={16}
-                        height={16}
-                        className="object-contain"
-                      />
-                    </div>
-                    <span>Explore</span>
-                  </div>
-                </Link>
-              </li>
-              <li
-                className={`mb-6   px-4  ${
-                  isActive([
-                    "/Appointment/Appointments",
-                    "/Appointment/CancelAppointment",
-                    "/Appointment/Appointment/Waiting",
-                    "/Appointment/SingleAppointment",
-                    "/Appointment/AppointmentDetail",
-                    "/Appointment/AppointentHistory",
-                    "/BookAppointment/",
-                  ]) && "border-l border-l-[var(--orange)]"
-                } `}
-              >
-                <Link href="/Appointment/Appointments">
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive([
-                        "/Appointment/Appointments",
-                        "/Appointment/CancelAppointment",
-                        "/Appointment/Waiting",
-                        "/Appointment/SingleAppointment",
-                        "/Appointment/AppointmentDetail",
-                        "/Appointment/AppointentHistory",
-                      ])
-                        ? "bg-[var(--orange)] text-white mx-3"
-                        : "text-[#202224] mx-3"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        src="/images/appoint.png"
-                        alt=" icon"
-                        width={16}
-                        height={16}
-                        className="object-contain"
-                      />
-                    </div>
-                    <span>Appointments</span>
-                  </div>
-                </Link>
-              </li>
-              <li
-                className={`mb-6   px-4  ${
-                  isActive("/Favorite") && "border-l border-l-[var(--orange)]"
-                } `}
-              >
-                <Link href="/Favorite">
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/Favorite")
-                        ? "bg-[var(--orange)] text-white mx-3"
-                        : "text-[#202224] mx-3"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        // src="/images/medication.png"
-                        src={
-                          isActive("/UserDashboard/Payment")
-                            ? "/images/paymentblue.svg"
-                            : "/images/Heart.png"
-                        }
-                        alt="Payment icon"
-                        width={20}
-                        height={20}
-                        className="object-contain"
-                      />
-                    </div>
-                    <span>Favorite</span>
-                  </div>
-                </Link>
-              </li>
-              <li
-                className={`mb-6   px-4  ${
-                  isActive("/Account") && "border-l border-l-[var(--orange)]"
-                } `}
-              >
-                <Link href="/Service/Account">
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/Service/Account")
-                        ? "bg-[var(--orange)] text-white mx-3"
-                        : "text-[#202224] mx-3"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        src={
-                          isActive("/Service/Account")
-                            ? "/images/accountwhite.png"
-                            : "/images/Account.png"
-                        }
-                        alt="Good Day/Bad Day icon"
-                        width={18}
-                        height={18}
-                      />
-                    </div>
-                    <span>Account</span>
-                  </div>
-                </Link>
-              </li>
-              <li
-                className={`mb-6   px-4  ${
-                  isActive("/Gift") && "border-l border-l-[var(--orange)]"
-                } `}
-              >
-                <Link href="/Points">
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/Points")
-                        ? "bg-[var(--orange)] text-white mx-3"
-                        : "text-[#202224] mx-3"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        // src="/images/medication.png"
-                        src={isActive("/Gift") ? "/gift.png" : "/giftwhite.png"}
-                        alt="Payment icon"
-                        width={20}
-                        height={20}
-                        className="object-contain"
-                      />
-                    </div>
-                    <span>Points</span>
-                  </div>
-                </Link>
-              </li>
-              <li
-                className={`mb-6   px-4  ${
-                  isActive("/StampCard") && "border-l border-l-[var(--orange)]"
-                } `}
-              >
-                <Link href="/StampCard">
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/StampCard")
-                        ? "bg-[var(--orange)] text-white mx-3"
-                        : "text-[#202224] mx-3"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        // src="/images/medication.png"
-                        src={
-                          isActive("/StampCard")
-                            ? "/stampwhite.png"
-                            : "/stampblack.png"
-                        }
-                        alt="Payment icon"
-                        width={24}
-                        height={24}
-                        className="object-contain"
-                      />
-                    </div>
-                    <span>Stamp Cards</span>
-                  </div>
-                </Link>
-              </li>
-              <li
-                className={`mb-6   px-4  ${
-                  isActive("/Gift") && "border-l border-l-[var(--orange)]"
-                } `}
-              >
-                <Link href="/Gift">
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/Gift")
-                        ? "bg-[var(--orange)] text-white mx-3"
-                        : "text-[#202224] mx-3"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        // src="/images/medication.png"
-                        src={
-                          isActive("/Gift") ? "/storewgite.png" : "/store.png"
-                        }
-                        alt="Payment icon"
-                        width={24}
-                        height={24}
-                        className="object-contain"
-                      />
-                    </div>
-                    <span>Gift Cards</span>
-                  </div>
-                </Link>
-              </li>
-            </ul>
-          </div>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
         </div>
       </div>
     </div>
--- Components/Modals/SidebarModal.js
+++ Components/Modals/SidebarModal.js
@@ -4,31 +4,88 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
-import { Menu, X } from "lucide-react";
+import { X } from "lucide-react";
 
 const SidebarModal = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
 
   const isActive = (paths) =>
     Array.isArray(paths) ? paths.includes(pathname) : pathname === paths;
 
-  const [isOpen, setIsOpen] = useState(false);
+  const menuItems = [
+    {
+      label: "Home",
+      href: "/HomeScreen",
+      icon: "/images/home.png",
+      activeIcon: "/images/home.png",
+    },
+    {
+      label: "Explore",
+      href: "/UserDashboard/Quotation",
+      icon: "/images/explore.png",
+      activeIcon: "/images/quotation.svg",
+    },
+    {
+      label: "Appointments",
+      href: "/Appointment/Appointments",
+      icon: "/images/appoint.png",
+      activeIcon: "/images/appointwhite.png",
+      match: [
+        "/Appointment/Appointments",
+        "/Appointment/CancelAppointment",
+        "/Appointment/Waiting",
+        "/Appointment/SingleAppointment",
+        "/Appointment/AppointmentDetail",
+        "/Appointment/AppointentHistory",
+      ],
+    },
+    {
+      label: "Favorite",
+      href: "/Favorite",
+      icon: "/images/Heart.png",
+      activeIcon: "/images/paymentblue.svg",
+    },
+    {
+      label: "Account",
+      href: "/Service/Account",
+      icon: "/images/Account.png",
+      activeIcon: "/images/accountwhite.png",
+    },
+    {
+      label: "Points",
+      href: "/Points",
+      icon: "/giftwhite.png",
+      activeIcon: "/gift.png",
+    },
+    {
+      label: "Stamp Cards",
+      href: "/StampCard",
+      icon: "/stampblack.png",
+      activeIcon: "/stampwhite.png",
+    },
+    {
+      label: "Gift Cards",
+      href: "/Gift",
+      icon: "/store.png",
+      activeIcon: "/storewgite.png",
+    },
+  ];
 
   return (
     <>
-      {/* Menu button to open modal */}
+      {/* Button to open modal */}
       <button onClick={() => setIsOpen(true)} aria-label="Open Menu">
         <div className="relative md:w-6 md:h-6 w-4 h-4">
-          {" "}
           <Image src="/images/menu.png" alt="menu" width={24} height={24} />
         </div>
       </button>
 
       {isOpen && (
-        <div className="fixed z-50 inset-0 bg-black/50 backdrop-blur-sm flex ">
-          {/* Side drawer */}
-          <div className="w-2/3 h-full bg-gray-50 p-4 rounded-r-xl overflow-y-auto">
-            {/* Header with close button */}
+        <div className="fixed z-50 inset-0 bg-black/50 backdrop-blur-sm flex">
+          {/* Sidebar Modal Content */}
+          <div className="w-2/3 h-full bg-white p-4 rounded-r-xl overflow-y-auto">
+            {/* Header */}
             <div className="flex justify-between items-center mb-6">
               <div className="flex items-center gap-2">
                 <Image src="/logo.png" alt="Logo" width={80} height={60} />
@@ -39,186 +96,45 @@ const SidebarModal = () => {
               </button>
             </div>
 
-            {/* Menu Links */}
+            {/* Navigation Items */}
             <ul>
-              <li className="mb-6">
-                <Link
-                  onClick={() => setIsOpen(false)}
-                  href="/UserDashboard/HomeScreen"
-                >
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/UserDashboard/HomeScreen")
-                        ? "bg-[var(--orange)] text-white"
-                        : "text-[#202224]"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        src="/images/home.png"
-                        alt="Pet Profile icon"
-                        width={18}
-                        height={18}
-                        className="object-contain"
-                      />
-                    </div>
-                    <span>Home</span>
-                  </div>
-                </Link>
-              </li>
+              {menuItems.map((item, index) => {
+                const active = isActive(item.match || item.href);
 
-              <li className="mb-6">
-                <Link
-                  onClick={() => setIsOpen(false)}
-                  href="/UserDashboard/Quotation"
-                >
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/UserDashboard/Quotation")
-                        ? "bg-[var(--orange)] text-white"
-                        : "text-[#202224]"
+                return (
+                  <li
+                    key={index}
+                    className={`mb-6 px-4 ${
+                      active && "border-l border-l-[var(--orange)]"
                     }`}
                   >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        src={
-                          isActive("/UserDashboard/Quotation")
-                            ? "/images/quotation.svg"
-                            : "/images/explore.png"
-                        }
-                        alt="Pet Profile icon"
-                        width={20}
-                        height={20}
-                        className="object-contain"
-                      />
-                    </div>
-                    <span>Explore</span>
-                  </div>
-                </Link>
-              </li>
-
-              <li
-                className={`mb-6 px-4 ${
-                  isActive([
-                    "/Appointments",
-                    "/CancelAppointment",
-                    "/Waiting",
-                    "/SingleAppointment",
-                    "/AppointmentDetail",
-                    "/AppointentHistory",
-                  ]) && "border-l border-l-[var(--orange)]"
-                }`}
-              >
-                <Link onClick={() => setIsOpen(false)} href="/Appointments">
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive([
-                        "/Appointments",
-                        "/CancelAppointment",
-                        "/Waiting",
-                        "/SingleAppointment",
-                        "/AppointmentDetail",
-                        "/AppointentHistory",
-                      ])
-                        ? "bg-[var(--orange)] text-white"
-                        : "text-[#202224]"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        src={
-                          isActive([
-                            "/Appointments",
-                            "/CancelAppointment",
-                            "/Waiting",
-                            "/SingleAppointment",
-                            "/AppointmentDetail",
-                            "/AppointentHistory",
-                          ])
-                            ? "/images/appointwhite.png"
-                            : "/images/appoint.png"
-                        }
-                        alt=" icon"
-                        width={20}
-                        height={20}
-                        className="object-contain"
-                      />
-                    </div>
-                    <span>Appointments</span>
-                  </div>
-                </Link>
-              </li>
-
-              <li
-                className={`mb-6 px-4 ${
-                  isActive("/Favorite") && "border-l border-l-[var(--orange)]"
-                }`}
-              >
-                <Link
-                  onClick={() => setIsOpen(false)}
-                  href="/UserDashboard/Payment"
-                >
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/UserDashboard/Payment")
-                        ? "bg-[var(--orange)] text-white"
-                        : "text-[#202224]"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        src={
-                          isActive("/UserDashboard/Payment")
-                            ? "/images/paymentblue.svg"
-                            : "/images/Heart.png"
-                        }
-                        alt="Payment icon"
-                        width={20}
-                        height={20}
-                        className="object-contain"
-                      />
-                    </div>
-                    <span>Favorite</span>
-                  </div>
-                </Link>
-              </li>
-
-              <li
-                className={`mb-6 px-4 ${
-                  isActive("/Account") && "border-l border-l-[var(--orange)]"
-                }`}
-              >
-                <Link
-                  onClick={() => setIsOpen(false)}
-                  href="/UserDashboard/Help"
-                >
-                  <div
-                    className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
-                      isActive("/UserDashboard/Help")
-                        ? "bg-[var(--orange)] text-white"
-                        : "text-[#202224]"
-                    }`}
-                  >
-                    <div className="mr-3 w-5 h-5 relative">
-                      <Image
-                        src={
-                          isActive("/UserDashboard/Help")
-                            ? "/images/helpblue.svg"
-                            : "/images/Account.png"
-                        }
-                        alt="Good Day/Bad Day icon"
-                        width={20}
-                        height={20}
-                      />
-                    </div>
-                    <span>Account</span>
-                  </div>
-                </Link>
-              </li>
+                    <Link href={item.href} onClick={() => setIsOpen(false)}>
+                      <div
+                        className={`flex items-center px-4 py-2 mb-1 rounded-md cursor-pointer transition-colors font-medium ${
+                          active
+                            ? "bg-[var(--orange)] text-white mx-3"
+                            : "text-[#202224] mx-3"
+                        }`}
+                      >
+                        <div className="mr-3 w-5 h-5 relative">
+                          <Image
+                            src={active ? item.activeIcon : item.icon}
+                            alt={`${item.label} icon`}
+                            width={20}
+                            height={20}
+                            className="object-contain"
+                          />
+                        </div>
+                        <span>{item.label}</span>
+                      </div>
+                    </Link>
+                  </li>
+                );
+              })}
             </ul>
           </div>
 
-          {/* clicking outside closes modal */}
+          {/* Close modal by clicking outside */}
           <div className="flex-1" onClick={() => setIsOpen(false)} />
         </div>
       )}
